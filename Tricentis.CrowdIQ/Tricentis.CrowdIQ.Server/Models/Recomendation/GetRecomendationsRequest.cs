d3518635a757424681c8598008b0221a95295d0a
dbcol <dbcol@DESKTOP-3RUT3U9>
2017-06-08 11:27:08 +1000

    Added transfer object

---
+++ Tricentis.CrowdIQ/Tricentis.CrowdIQ.Server/Models/Recomendation/GetRecomendationsRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Tricentis.CrowdIQ.Server.Models.Recomendation
+{
+
+    public class GetRecomendationsRequest
+    {
+        [JsonProperty(Required =Required.Always)]
+        public string engine { get; set; }
+    }
+}
